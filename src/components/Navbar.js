a5733c529ffe073b5a5367a5de5cf591278ec956
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-07-15 13:50:11 +0530

    Change accent color.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,12 @@ const Navbar = () => {
                 fillRule='evenodd'
                 clipRule='evenodd'
                 d='M0 20c0 11.046 8.954 20 20 20s20-8.954 20-20S31.046 0 20 0 0 8.954 0 20Zm20 16c-7.264 0-13.321-5.163-14.704-12.02C4.97 22.358 6.343 21 8 21h24c1.657 0 3.031 1.357 2.704 2.98C33.32 30.838 27.264 36 20 36Z'
-                fill='#4ade80'
+                fill='#7c3aed'
               ></path>
             </svg>
 
             <span className='font-display text-xl'>
-              Todo<span className='font-bold text-emerald-500'>Pal</span>
+              Todo<span className='font-bold text-violet-600'>Pal</span>
             </span>
           </div>
 
